cf8f60eb0eac8cf7ed1c01455a92399795e8cbb1
Okpola Ugbolochi .S <88292390+linto94@users.noreply.github.com>
2022-10-28 18:55:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
-const modal = document.querySelector(".modal"); 
-const modalContent = document.querySelector(".modal-content");
-const btn = document.querySelector(".btn"); 
-const close = document.querySelector(".close"); 
+const number = document.querySelector(".number");
+const btn = document.querySelector(".generate");
 
+btn.addEventListener("click", generateNumber);
+window.addEventListener("load", generateNumber);
 
-btn.addEventListener("click", openModal);
-close.addEventListener("click", closeModal);
-modal.addEventListener("click", closeModal);
-
-
-// OPEN MODAL
-function openModal(e) {
-    e.preventDefault();
-    modal.style.display = "block";
-}
-
-
-// CLOSE MODAL
-function closeModal() {
-    modalContent.classList.add("slide-up");
-    
-    setTimeout(() => {
-        modal.style.display = "none";
-        modalContent.classList.remove("slide-up");  
-    }, 500)
+function generateNumber() {
+  const randomNumber = Math.floor(Math.random() * 10 + 1);
+  number.innerHTML = randomNumber;
 }
