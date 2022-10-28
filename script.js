ae94b26aa3455e4f6abfd07a45f8cad5dd4c5584
Okpola Ugbolochi .S <88292390+linto94@users.noreply.github.com>
2022-10-28 18:53:49 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
-const modal = document.querySelector(".modal"); 
-const modalContent = document.querySelector(".modal-content");
-const btn = document.querySelector(".btn"); 
-const close = document.querySelector(".close"); 
+// let randomColor = Math.random();
+// randomColor = Math.random().toString(16);
+// randomColor = Math.random().toString(16).substring(2, 8);
+// console.log(randomColor);
 
+const hex = document.querySelector(".hex");
+const btn = document.querySelector(".generate");
+const copy = document.querySelector(".copy");
 
-btn.addEventListener("click", openModal);
-close.addEventListener("click", closeModal);
-modal.addEventListener("click", closeModal);
+const generateColor = function () {
+  const randomColor = Math.random().toString(16).substring(2, 8);
+  hex.value = "#" + randomColor;
+  document.body.style.background = hex.value;
+};
 
+const copyColor = function () {
+  hex.select();
+  hex.setSelectionRange(0, 1000);
+  document.execCommand("copy");
+  alert("Color has been copied to the clipboard");
+};
 
-// OPEN MODAL
-function openModal(e) {
-    e.preventDefault();
-    modal.style.display = "block";
-}
-
-
-// CLOSE MODAL
-function closeModal() {
-    modalContent.classList.add("slide-up");
-    
-    setTimeout(() => {
-        modal.style.display = "none";
-        modalContent.classList.remove("slide-up");  
-    }, 500)
-}
+btn.addEventListener("click", generateColor);
+copy.addEventListener("click", copyColor);
