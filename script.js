d297f81c7423cfa5dae5e107beec3477b73e4c15
Okpola Ugbolochi .S <88292390+linto94@users.noreply.github.com>
2022-10-28 18:51:47 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
-const modal = document.querySelector(".modal"); 
-const modalContent = document.querySelector(".modal-content");
-const btn = document.querySelector(".btn"); 
-const close = document.querySelector(".close"); 
+const count = document.querySelector(".count");
+const buttons= document.querySelector(".buttons");
+// const add = document.querySelector(".add");
+// const resetCount = document.querySelector(".reset");
+// const substract = document.querySelector(".substract");
 
+// add.addEventListener("click", function () {
+//     count.innerHTML++;
+// });
 
-btn.addEventListener("click", openModal);
-close.addEventListener("click", closeModal);
-modal.addEventListener("click", closeModal);
+// substract.addEventListener("click", function () {
+//     count.innerHTML--;
+// });
 
+// resetCount.addEventListener("click", function () {
+//     count.innerHTML = 0;
+// });
 
-// OPEN MODAL
-function openModal(e) {
-    e.preventDefault();
-    modal.style.display = "block";
+buttons.addEventListener("click", function (e) {
+    if (e.target.classList.contains("add")) {
+        count.innerHTML++;
+        setColor();
+    } else if (e.target.classList.contains("substract")) {
+        count.innerHTML--;
+        setColor();
+    } else if (e.target.classList.contains("reset")) {
+        count.innerHTML = 0;
+        setColor();
+    }
+});
+
+function setColor() {
+    if (count.innerHTML > 0) {
+        count.style.borderColor = "lightgreen";
+    } else if (count.innerHTML < 0){
+        count.style.borderColor = "red";
+    } else{
+        count.style.borderColor = "white";
+    }
 }
 
 
-// CLOSE MODAL
-function closeModal() {
-    modalContent.classList.add("slide-up");
-    
-    setTimeout(() => {
-        modal.style.display = "none";
-        modalContent.classList.remove("slide-up");  
-    }, 500)
-}
+
