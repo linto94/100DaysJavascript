50a66913c9c5d1d3fa3ba835828c80241249465b
Okpola Ugbolochi .S <88292390+linto94@users.noreply.github.com>
2022-11-22 18:26:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const btn = document.querySelector(".btn");
+const tip = document.querySelector(".tip");
+const total = document.querySelector(".total");
+const error = document.querySelector(".error");
+
+
+const hideError = () => {
+    setTimeout(() =>{
+        error.style.display = "none";
+    }, 5000);
+};
+
+const calculateTip = () => {
+    const bill = document.querySelector(".bill").value;
+    const rate = document.querySelector(".rate").value;
+     
+
+
+    if (bill === "" || rate == "" ) {
+        // console.log("please add a number");
+        error.style.display = "block";
+        hideError();
+    } else if (isNaN(bill)) {
+        error.innerHTML = "Please enter a number";
+        error.style.display = "block";
+        hideError();
+    } else {
+        let tipAmt = bill * rate;
+        tipAmt = Math.ceil(tipAmt);
+        tip.innerHTML = `Tip: $${tipAmt}`;
+
+        let totalbill = Number(bill) + tipAmt;
+        total.innerHTML = `Total Bill: $${totalbill}`;
+    }
+};
+
+btn.addEventListener("click", calculateTip);
