ae6f13218e97a59ea4e0721b6cd712beb55bf2a8
Okpola Ugbolochi .S <88292390+linto94@users.noreply.github.com>
2022-12-31 10:32:16 +0100

    Add files via upload

---
+++ music.js
@@ -0,0 +1,26 @@
+let trackList = [
+    {
+    	name: "Many Ways",
+    	path: "./music/Buju.mp3",
+    	img: "./images/many.jpg",
+    	singer: "BNXN fka Buju",
+    },
+    {
+    	name: "Understand",
+    	path: "./music/Omahlay.mp3",
+    	img: "./images/understand.jpeg",
+    	singer: "OmahLay",
+    },
+    {
+    	name: "Girlfriend",
+    	path: "./music/Ruger.mp3",
+    	img: "./images/Girlfriend.jpg",
+    	singer: "Ruger",
+    },
+    {
+    	name: "Gbese",
+    	path: "./music/Wizkid.mp3",
+    	img: "./images/gbese.jpg",
+    	singer: "Wizkid",
+    },
+];
