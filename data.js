cdb9decac831feab29906312c3e00f2ad444a3bd
Okpola Ugbolochi .S <88292390+linto94@users.noreply.github.com>
2023-01-14 19:20:55 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,18 @@
+let lists = [
+    {
+        question: "what is your name",
+        answer: "my name is Anita.",
+    },
+    {
+        question: "what's your name",
+        answer: "my name is Anita",
+    },
+    {
+        question: "what to do you",
+        answer: "i assist developers write better code",
+    },
+    {
+        question: "introduce yourself",
+        answer: "my name is Anita, I help developers write better code ",
+    },
+];
